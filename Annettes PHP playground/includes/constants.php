bac42854cff3392c9ca55d4fcc784ae3f687ba26
annetterc <annetteschoeffel@gmail.com>
2017-03-24 13:12:22 -0400

    Created by Coding Ground Client

---
+++ Annettes PHP playground/includes/constants.php
@@ -0,0 +1,27 @@
+<?php
+//define('primarysubstance', array(   -- use this line with PHP7   --- wrap all others in it too
+const primarysubstance = array(
+    'primarysubstance',
+    'prisubstance_alcohol', 
+    'prisubstance_marijuana', 
+    'prisubstance_cocaine',
+    'prisubstance_meth',
+    'prisubstance_heroin',
+    'prisubstance_otheropiates',
+    'prisubstance_nonpresc',
+    'prisubstance_pcp',
+    'prisubstance_syn',
+    'prisubstance_otherhallucinogen',
+    'prisubstance_otheramphetamine',
+    'prisubstance_otherstim',
+    'prisubstance_benzo',
+    'prisubstance_othernonbenzo',
+    'prisubstance_barbit',
+    'prisubstance_othernonbarbit',
+    'prisubstance_inhalant',
+    'prisubstance_otc',
+    'prisubstance_other',
+    'prisubstance_other_i'
+    );
+//  ));  -- use this line for PHP7
+?>
